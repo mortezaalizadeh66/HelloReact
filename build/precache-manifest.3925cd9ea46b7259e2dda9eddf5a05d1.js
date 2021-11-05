5199483e3753a28d5983c4a004fb04c12c3b513d
mortezaalizadeh66 <65837442+mortezaalizadeh66@users.noreply.github.com>
2021-11-05 11:57:14 +0100

    Add files via upload

---
+++ build/precache-manifest.3925cd9ea46b7259e2dda9eddf5a05d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc28495e6216e2661ae1dca3421401b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9e3abd3d294f6165624",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "5a46617966b536919ab2",
+    "url": "/static/js/2.68f56c50.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.68f56c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e3abd3d294f6165624",
+    "url": "/static/js/main.fc1cb0dc.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
